986dfb726cb7cbfdb486f14c35f65bede1510257
Nikhil <nikhiljangra264@gmail.com>
2023-10-23 13:43:44 +0530

    updated gitignore and heap_sort

--- sorting/heap_sort.py
+++ sorting/heap_sort.py
@@ -3,36 +3,37 @@
 # extract the max element and again heapify the tree
 # worst time complexity n*lg(n)
 
-def max_heapify(arr, parent, size):
-    # Calculate the indices of the left and right child nodes
-    left = parent * 2 + 1
-    right = parent * 2 + 2
-
-    # Initialize the largest value as the current parent
-    largest = parent
-
-    # Check if the left child exists and is greater than the parent
-    if left < size and arr[left] > arr[parent]:
-        largest = left
-        
-    # Check if the right child exists and is greater than the largest (which could be the parent or left child)
-    if right < size and arr[right] > arr[largest]:
-        largest = right
-
-    # If the largest value is not the parent, swap the parent and largest values
-    if parent != largest:
-        arr[parent], arr[largest] = arr[largest], arr[parent]
-        # Recursively call max_heapify on the affected child node (largest)
-        max_heapify(arr, largest, size)
-
-def build_max_heap(arr):
-    # Start from the middle of the array and work towards the beginning
-    for index in range(len(arr)//2, -1, -1):
-        # Call max_heapify on each index, building the Max Heap
-        max_heapify(arr, index, len(arr))
-
 def heap_sort(arr):
     # Build a Max Heap from the input array
+
+    def max_heapify(arr, parent, size):
+        # Calculate the indices of the left and right child nodes
+        left = parent * 2 + 1
+        right = parent * 2 + 2
+
+        # Initialize the largest value as the current parent
+        largest = parent
+
+        # Check if the left child exists and is greater than the parent
+        if left < size and arr[left] > arr[parent]:
+            largest = left
+            
+        # Check if the right child exists and is greater than the largest (which could be the parent or left child)
+        if right < size and arr[right] > arr[largest]:
+            largest = right
+
+        # If the largest value is not the parent, swap the parent and largest values
+        if parent != largest:
+            arr[parent], arr[largest] = arr[largest], arr[parent]
+            # Recursively call max_heapify on the affected child node (largest)
+            max_heapify(arr, largest, size)
+    
+    def build_max_heap(arr):
+        # Start from the middle of the array and work towards the beginning
+        for index in range(len(arr)//2, -1, -1):
+            # Call max_heapify on each index, building the Max Heap
+            max_heapify(arr, index, len(arr))
+
     build_max_heap(arr)
     # Iterate from the end of the array to the beginning
     for index in range(len(arr)-1, -1, -1):
